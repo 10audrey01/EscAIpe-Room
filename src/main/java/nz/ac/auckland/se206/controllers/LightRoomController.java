ae814289e70e99fd37e0b931e783c0771481f980
audrey <ayou332@aucklanduni.ac.nz>
2023-08-01 17:38:33 +1200

    Added game dialogue pane with yes or no buttons

--- src/main/java/nz/ac/auckland/se206/controllers/LightRoomController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/LightRoomController.java
@@ -2,16 +2,33 @@ package nz.ac.auckland.se206.controllers;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
 import nz.ac.auckland.se206.App;
 
 public class LightRoomController {
 
   @FXML private Rectangle window;
+  @FXML private Pane gameDialogue;
+  @FXML private Label itemLabel;
+  @FXML private Button yesButton;
+  @FXML private Button noButton;
 
   @FXML
-  public void clickWindow() throws IOException {
-    System.out.println("Window clicked");
+  public void onClickWindow() {
+    itemLabel.setText("   Draw the blinds?");
+    gameDialogue.setVisible(true);
+  }
+
+  @FXML
+  public void onClickYes() throws IOException {
     App.setRoot("darkRoom");
+  }
+
+  @FXML
+  public void onClickNo() {
+    gameDialogue.setVisible(false);
   }
 }

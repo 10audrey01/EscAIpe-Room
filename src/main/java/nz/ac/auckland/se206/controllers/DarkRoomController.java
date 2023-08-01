3a2e97c1102507e23cc8b62aed00e82c67474858
audrey <ayou332@aucklanduni.ac.nz>
2023-08-01 18:22:59 +1200

    Added option to go back to light room

--- src/main/java/nz/ac/auckland/se206/controllers/DarkRoomController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/DarkRoomController.java
@@ -1,6 +1,10 @@
 package nz.ac.auckland.se206.controllers;
 
+import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.GameState;
@@ -9,11 +13,32 @@ import nz.ac.auckland.se206.SceneManager.AppUi;
 public class DarkRoomController {
 
   @FXML private Rectangle riddleBook;
+  @FXML private Rectangle window;
+  @FXML private Pane gameDialogue;
+  @FXML private Label itemLabel;
+  @FXML private Button yesButton;
+  @FXML private Button noButton;
 
   @FXML
   public void onClickBook() {
     if (!GameState.isRiddleResolved) {
       App.setUi(AppUi.CHAT);
     }
+  }
+
+  @FXML
+  public void onClickWindow() {
+    itemLabel.setText("   Open the blinds?");
+    gameDialogue.setVisible(true);
+  }
+
+  @FXML
+  public void onClickYes() throws IOException {
+    App.setRoot("lightRoom");
+  }
+
+  @FXML
+  public void onClickNo() {
+    gameDialogue.setVisible(false);
   }
 }

8c04bf3dd59dbdc346063765c46c16ef1a8ab6c2
audrey <ayou332@aucklanduni.ac.nz>
2023-08-01 16:57:41 +1200

    Added dark room fxml and controller

--- src/main/java/nz/ac/auckland/se206/controllers/LightRoomController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/LightRoomController.java
@@ -1,3 +1,17 @@
 package nz.ac.auckland.se206.controllers;
 
-public class LightRoomController {}
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.shape.Rectangle;
+import nz.ac.auckland.se206.App;
+
+public class LightRoomController {
+
+  @FXML private Rectangle window;
+
+  @FXML
+  public void clickWindow() throws IOException {
+    System.out.println("Window clicked");
+    App.setRoot("darkRoom");
+  }
+}

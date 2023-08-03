61954f39c273686246cb352e6353fedd561d503d
audrey <ayou332@aucklanduni.ac.nz>
2023-08-03 19:13:28 +1200

    Added music for if you escaped and bug fixes if user plays again

--- src/main/java/nz/ac/auckland/se206/controllers/EndPageController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/EndPageController.java
@@ -1,9 +1,13 @@
 package nz.ac.auckland.se206.controllers;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.GameState;
 
@@ -13,14 +17,26 @@ public class EndPageController {
   @FXML private Button playAgainButton;
   @FXML private Button returnToMainMeuButton;
 
+  private MediaPlayer escapePlayer;
+
   @FXML
-  public void initialize() {
+  public void initialize() throws URISyntaxException {
     if (LightRoomController.getVinylMediaPlayer() != null) {
       LightRoomController.getVinylMediaPlayer().stop();
     }
 
     if (GameState.isEscaped) {
       endLabel.setText("You escaped the room!");
+      escapePlayer =
+          new MediaPlayer(
+              new Media(getClass().getResource("/sounds/escape.mp3").toURI().toString()));
+      escapePlayer.setOnEndOfMedia(
+          new Runnable() {
+            public void run() {
+              escapePlayer.seek(Duration.ZERO);
+            }
+          });
+      escapePlayer.play();
     } else {
       endLabel.setText("You failed to escape the room!");
     }
@@ -34,6 +50,9 @@ public class EndPageController {
     GameState.isVinylPlayed = false;
     GameState.isEscaped = false;
 
+    if (escapePlayer != null) {
+      escapePlayer.stop();
+    }
     StartPageController.getMusicPlayer().play();
 
     App.setRoot("lightRoom");
@@ -41,6 +60,15 @@ public class EndPageController {
 
   @FXML
   public void onClickReturnToMainMenu() throws IOException {
+    GameState.isRiddleResolved = false;
+    GameState.isKeyFound = false;
+    GameState.isVinylFound = false;
+    GameState.isVinylPlayed = false;
+    GameState.isEscaped = false;
+
+    if (escapePlayer != null) {
+      escapePlayer.stop();
+    }
     App.setRoot("startPage");
   }
 }
